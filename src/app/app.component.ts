b0840aba3e408926f4b0c59e5a43f41a92207b78
francoode <francoode@gmail.com>
2020-11-21 16:48:34 -0300

    array palabras

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,111 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    palabra = 'VENTILADOR';
-    palabraOculta = '';
-    intentos = 0;
+  end = false;
 
-    gano = false;
-    perdio = false;
+  palabra = '';
+  pistas = [];
+  palabraOculta = '';
+  index = 0;
+
+  palabras = [
+    {
+      palabra: 'hola',
+      pistas: ['pista1', 'pista2']
+    },
+    {
+      palabra: 'chau',
+      pistas: ['123', '123']
+    },
+    {
+      palabra: 'qtal',
+      pistas: ['xx', 'dd']
+    }
+  ];
 
 
-    letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-              'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
-              'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  intentos = 0;
 
-       
-  constructor () {
-    
-    this.palabraOculta = '_ '.repeat (this.palabra.length);
+  gano = false;
+  perdio = false;
+
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+  constructor() {
   }
-  
-  comprobar (letra) {
 
-    this.existeLetra (letra);
-    let palabraOcultaArr = this.palabraOculta.split(' ');
-   
-    for (let i=0; i< this.palabra.length; i++){
-      
-      if ( this.palabra[i] === letra ) {
-      palabraOcultaArr[i] = letra;
+  ngOnInit() {
+    this.palabra = this.palabras[this.index].palabra.toUpperCase();
+    this.pistas = this.palabras[this.index].pistas;
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+  }
+
+  comprobar(letra) {
+    this.existeLetra(letra);
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
       }
-   
     }
 
-      this.palabraOculta = palabraOcultaArr.join (' ');
-      this.verificaGane();    
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
   }
 
-      verificaGane() {
+  verificaGane() {
 
-        const palabraArr = this.palabraOculta.split (' ');
-        const palabraEvaluar = palabraArr.join('');
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
 
-        if (palabraEvaluar === this.palabra) {
-          this.gano = true;
-          console.log ('Usuario GANO');
-        } 
-
-        if (this.intentos >= 9) {
-          this.perdio= true;
-          console.log('Usuario perdió')
-        }
-
-      }
-
-      existeLetra (letra) {
-        if (this.palabra.indexOf( letra)  >= 0 ){
-          
-          //console.log ('La letra existe ' + letra);
-        } else {
-          //console.log ('La letra NO existe ' + letra);
-          this.intentos ++;
-        }
+    if (palabraEvaluar === this.palabra) {
+      this.gano = true;
+      this.resetPalabras('win');
+      console.log('Usuario GANO');
     }
-  
+
+    if (this.intentos >= 9) {
+      this.perdio = true;
+      this.resetPalabras('lose');
+      console.log('Usuario perdió');
+    }
+
+  }
+
+  existeLetra(letra) {
+    if (!(this.palabra.indexOf(letra) >= 0)) {
+      this.intentos++;
+    }
+  }
+
+  resetPalabras(type) {
+    setTimeout(() => {
+      if ((this.index + 1) !== this.palabras.length) {
+        this.index = this.index + 1;
+        this.palabra = this.palabras[this.index].palabra.toUpperCase();
+        this.palabraOculta = '_ '.repeat(this.palabra.length);
+        this.pistas = [];
+        this.pistas = this.palabras[this.index].pistas;
+
+        this.intentos = 0;
+        this.gano = false;
+        this.perdio = false;
+      } else {
+        this.end = true;
+      }
+    }, 3000);
+  }
+
 
 }
