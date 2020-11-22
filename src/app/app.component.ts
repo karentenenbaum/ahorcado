5944f4a2b8d2991d98fb1646836b2afff8cb8a3f
francoode <francoode@gmail.com>
2020-11-22 12:26:03 -0300

    letras incorrectas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,50 @@ export class AppComponent implements OnInit {
 
   palabra = '';
   pistas = [];
+  selected = [];
   palabraOculta = '';
   index = 0;
 
   palabras = [
     {
-      palabra: 'hola',
-      pistas: ['pista1', 'pista2']
+      palabra: 'FISICAS',
+      pistas: ['Estas personas comienzan a existir desde que son concebidas en el seno materno']
     },
     {
-      palabra: 'chau',
-      pistas: ['123', '123']
+      palabra: 'JURIDICAS',
+      pistas: ['Estas personas nacen de acuerdo a un acto jurídico o acto de constitución, o sea,' +
+      ' un evento jurídico de fundación que es reconocido por los organismos y autoridades administrativas,']
     },
     {
-      palabra: 'qtal',
-      pistas: ['xx', 'dd']
-    }
+      palabra: 'PATRIMONIO',
+      pistas: ['Atributo de la personalidad que se define como el conjunto de bienes y obligaciones pertenecientes a una persona natural o jurídica' +
+      'que son susceptibles de ser valorados economicamente']
+    },
+    {
+      palabra: 'CAPACIDAD',
+      pistas: ['Atributo de la personalidad que consiste en la aptitud de contraer derechos y obligaciones']
+    },
+    {
+      palabra: 'ESTADO',
+      pistas: ['Atributo de la personalidad que solo tienen las personas de existencia visible']
+    },
+    {
+      palabra: 'MUEBLES',
+      pistas: ['Clasificación de las cosas: Son aquellos bienes que por su naturaleza pueden transportarse de un lugar a otro.']
+    },
+    {
+      palabra: 'CONTRATO',
+      pistas: ['Acuerdo entre dos o más personas sobre una declaración de voluntad común destinada a reglar sus derechos']
+    },
+    {
+      palabra: 'ACREEDOR',
+      pistas: ['Parte de un contrato que tiene derecho a pedir que se cumpla una obligación, especialmente que se le pague una deuda.']
+    },
+    {
+      palabra: 'OBLIGACION',
+      pistas: ['Vínculo o relación jurídica en virtud de la cual una persona ( acreedor) tiene la facultad de exigir de otra ( deudor)' +
+      ' un determinado comportamiento positivo o negativo ( prestación), de cuyo cumplimiento responderá en última instancia el patrimonio del deudor.']
+    },
   ];
 
 
@@ -85,6 +113,7 @@ export class AppComponent implements OnInit {
 
   existeLetra(letra) {
     if (!(this.palabra.indexOf(letra) >= 0)) {
+      this.selected.push(letra);
       this.intentos++;
     }
   }
@@ -97,6 +126,7 @@ export class AppComponent implements OnInit {
         this.palabraOculta = '_ '.repeat(this.palabra.length);
         this.pistas = [];
         this.pistas = this.palabras[this.index].pistas;
+        this.selected = [];
 
         this.intentos = 0;
         this.gano = false;
@@ -104,7 +134,7 @@ export class AppComponent implements OnInit {
       } else {
         this.end = true;
       }
-    }, 3000);
+    }, 2000);
   }
 
 
